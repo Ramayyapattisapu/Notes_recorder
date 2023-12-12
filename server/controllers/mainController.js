f0b2b21b89de85ccb6b688946bbd20229a45c497
Ramayyapattisapu <ramayyapattisapu@gmail.com>
2023-12-12 20:58:00 +0530

    initial setup

---
+++ server/controllers/mainController.js
@@ -0,0 +1,16 @@
+exports.homepage = async (req, res) => {
+    const locals = {
+        title: "NodeJs Notes",
+        description: "Free NodeJs Notes App.",
+    }
+    res.render('index', locals);
+}
+
+
+exports.about = async (req, res) => {
+    const locals = {
+        title: "About - NodeJs Notes",
+        description: "Free NodeJs Notes App.",
+    }
+    res.render('about', locals);
+}
