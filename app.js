02b5458cd1c26c1fb80c985501fefae2f6f734b9
Ramayyapattisapu <ramayyapattisapu@gmail.com>
2023-12-14 22:11:40 +0530

    connected to mongodb

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ require("dotenv").config();
 
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const connectDB = require('./server/config/db');
+const session = require('express-session');
+const passport = require('passport');
+const MongoStore = require('connect-mongo');
 
 const app = express();
 const port = 8000 || process.env.PORT;
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//connect to db
+connectDB();
+
 
 // static files
 app.use(express.static("public"));
@@ -19,6 +30,7 @@ app.set("view engine", "ejs");
 
 
 //Routes
+app.use('/', require('./server/routes/auth'));
 app.use('/', require('./server/routes/index'));
 app.use('/', require('./server/routes/dashboard'));
 
