db406c59ae3e3fc870430131ffbc017b34eaf241
Ramayyapattisapu <ramayyapattisapu@gmail.com>
2023-12-15 20:26:03 +0530

    added o-auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,19 +1,83 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth20').Strategy;
+const passport = require("passport");
+var GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/User");
 
-passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://www.example.com/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-));
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    async function (accessToken, refreshToken, profile, done) {
+      const newUser = {
+        googleId: profile.id,
+        displayName: profile.displayName,
+        firstName: profile.name.givenName,
+        lasName: profile.name.familyName,
+        profileImage: profile.photos[0].value,
+      };
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+        if (user) {
+          done(null, user);
+        } else {
+          user = await User.create(newUser);
+          done(null, user);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  )
+);
 
+//Google login route
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+//retrive user data
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/login-failure",
+    successRedirect: "/dashboard",
+  })
+);
+
+// Destroy user session
+router.get("/logout", (req, res) => {
+  req.session.destroy((error) => {
+    if (error) {
+      console.log(error);
+      res.send("Error logging out");
+    } else {
+      res.redirect("/");
+    }
+  });
+});
+
+//route if something goes wrong
+router.get("/login-failure", (req, res) => {
+  res.send("Something went wrong...");
+});
+
+// persist user data after successful authentication
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+// retrive user data from session
+
+passport.deserializeUser(function (user, done) {
+  user.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
 
 module.exports = router;
