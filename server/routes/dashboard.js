56d6e876a256f6eaa24357b3b97514a306357fe1
Ramayyapattisapu <ramayyapattisapu@gmail.com>
2023-12-18 18:09:40 +0530

    delete-note

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -8,6 +8,7 @@ const { isLoggedIn } = require('../middleware/checkAuth');
 router.get('/dashboard', isLoggedIn, dashboardController.dashboard);
 router.get('/dashboard/item/:id', isLoggedIn, dashboardController.dashboardViewNote);
 router.put('/dashboard/item/:id', isLoggedIn, dashboardController.dashboardUpdateNote);
+router.delete('/dashboard/item-delete/:id', isLoggedIn, dashboardController.dashboardDeleteNote);
 
 
-module.exports = router;  ``
+module.exports = router;
